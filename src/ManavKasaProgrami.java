3664f16b1d2cc1a1d7ec5b4fdc837b8be6a22950
Onur Cura <onurcuraa@gmail.com>
2023-04-08 14:39:46 +0300

    Proje yapıldı.

---
+++ src/ManavKasaProgrami.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ManavKasaProgrami {
+    public static void main(String[] args) {
+        //Değişkenleri tanımladık
+        double armutKg,elmaKg,domKg,muzKg,patKg,armut=2.14,elma=3.67,dom=1.11,muz=0.95,pat=5.0,tutar;
+        Scanner inp = new Scanner(System.in);
+
+        //Kullanıcıdan aldıkları meyvelerin kilosunu aldık.
+        System.out.println("Hoşgeldiniz! Lütfen Aldığınız Ürünlerin Kaç Kilogramın Olduğunu Giriniz : ");
+        System.out.print("Armut : ");
+        armutKg = inp.nextDouble();
+        System.out.print("Elma : ");
+        elmaKg = inp.nextDouble();
+        System.out.print("Domates : ");
+        domKg = inp.nextDouble();
+        System.out.print("Muz : ");
+        muzKg = inp.nextDouble();
+        System.out.print("Patlıcan : ");
+        patKg= inp.nextDouble();
+
+        //Toplam tutarı hesaplayarak yazdırdık.
+        tutar=armut*armutKg + elma*elmaKg + dom*domKg + muz*muzKg + pat*patKg;
+        System.out.print("Alışveriş Tutarınız : " +tutar + " TL");
+
+
+    }
+}

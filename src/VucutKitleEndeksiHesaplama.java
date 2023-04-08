6fd555bb241d8b40885ca89ae2a1984faa41ab2d
Onur Cura <onurcuraa@gmail.com>
2023-04-08 14:27:31 +0300

    Proje yapıldı.

---
+++ src/VucutKitleEndeksiHesaplama.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class VucutKitleEndeksiHesaplama {
+    public static void main(String[] args) {
+        //Değişkenleri tanımladık.
+        double size,weight,indeks;
+        Scanner inp = new Scanner(System.in);
+
+        //Kullanıcıdan boy ve kiloyu aldık.
+        System.out.print("Lütfen Boyunuzu Metre Cinsinden Giriniz : ");
+        size = inp.nextDouble();
+        System.out.print("Lütfen Kilonuzu Kilogram Cinsinden Giriniz : ");
+        weight = inp.nextDouble();
+
+        //Vücut kitle indeksiniz hesaplayıp yazdırdık.
+        indeks = weight/(size*size);
+        System.out.println("Vücut Kitle Endeksiniz : " + indeks);
+
+    }
+}

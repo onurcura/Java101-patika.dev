677873a7e50c8cc659a35c7af2a9eb7021786a74
Onur Cura <onurcuraa@gmail.com>
2023-04-11 01:53:32 +0300

    Proje yapıldı.

---
+++ src/Donguler/AsalSayiBulma.java
@@ -0,0 +1,28 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class AsalSayiBulma {
+    public static void main(String[] args) {
+        int num;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Lütfen Bir Sayı Giriniz : ");
+        num=inp.nextInt();
+        System.out.print("Asal Sayılar : ");
+        for (int i=1;i<=num;i++){
+            int total=0;
+            for (int k=1;k<i;k++){
+                if (i%k==0){
+                    total+=k;
+                }
+            }
+            if (total==1){
+                System.out.print(i +",");
+            }
+        }
+        System.out.print("\b" + ".");
+
+
+    }
+}

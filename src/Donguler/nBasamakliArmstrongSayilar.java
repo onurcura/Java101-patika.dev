ec3ad3cb8a57eb1376761e716daac222e076945b
Onur Cura <onurcuraa@gmail.com>
2023-04-10 15:44:58 +0300

    Proje yapıldı.

---
+++ src/Donguler/nBasamakliArmstrongSayilar.java
@@ -0,0 +1,41 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class nBasamakliArmstrongSayilar {
+    public static void main(String[] args) {
+        int number1 = 1, number2 = 1, tempNumber, digNumber, digValue, total, result;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Lütfen Bulmak İstediğiniz Armstrong Sayılarının Kaç Basamaklı Olacağını Giriniz : ");
+        digNumber = inp.nextInt();
+        for (int i = 1; i <= digNumber; i++) {
+            number1 *= 10;
+            if (i < digNumber) {
+                number2 *= 10;
+            }
+        }
+
+        System.out.print(digNumber + " Basamaklı Armstrong Sayıları : ");
+        for (int i = number2; i < number1; i++) {
+            result = 0;
+            tempNumber = i;
+            while (tempNumber != 0) {
+                digValue = tempNumber % 10;
+                total = 1;
+                for (int j = 1; j <= digNumber; j++) {
+                    total *= digValue;
+                }
+                result += total;
+                tempNumber /= 10;
+
+            }
+            if (result == i) {
+                System.out.print(i + ",");
+            }
+
+        }
+        System.out.print("\b" + ".");
+
+    }
+}

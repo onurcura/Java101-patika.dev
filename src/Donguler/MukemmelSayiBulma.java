92a48c90b2bec2866800fb11f78e85692e7fd7a8
Onur Cura <onurcuraa@gmail.com>
2023-04-11 01:05:09 +0300

    Proje yapıldı.

--- src/Donguler/MukemmelSayiBulma.java
+++ src/Donguler/MukemmelSayiBulma.java
@@ -21,6 +21,8 @@ public class MukemmelSayiBulma {
             System.out.print(num + " Bir Mükemmel Sayı Değildir.");
         }
 
+        //Bir sayının kendisi hariç pozitif tam sayı çarpanları (kalansız bölen sayıların) toplamı kendisine eşit olan sayıya mükemmel sayı denir.
+
     }
 }
 

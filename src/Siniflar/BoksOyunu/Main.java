49808c509c401169d638d60de5a0d289a1bb22d4
Onur Cura <onurcuraa@gmail.com>
2023-04-14 19:05:44 +0300

    Proje yapıldı.

--- src/Siniflar/BoksOyunu/Main.java
+++ src/Siniflar/BoksOyunu/Main.java
@@ -2,8 +2,8 @@ package Siniflar.BoksOyunu;
 
 public class Main {
     public static void main(String[] args) {
-        Fighter f1 = new Fighter("Ali",10,120,92,50,25);
-        Fighter f2 = new Fighter("Frazier",15,140,100,15,5);
+        Fighter f1 = new Fighter("Ali",10,120,92,50,50);
+        Fighter f2 = new Fighter("Frazier",15,140,100,25,10);
 
         Match match = new Match(f1,f2,90,100);
         match.run();

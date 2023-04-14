f8cb5727de11e66c05867ffa35fa429f6c33ae17
Onur Cura <onurcuraa@gmail.com>
2023-04-14 23:42:29 +0300

    Proje yapıldı.

---
+++ src/Siniflar/MaasHesaplayici/Main.java
@@ -0,0 +1,12 @@
+package Siniflar.MaasHesaplayici;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee e1 = new Employee("Onur",2000,40,2005);
+        Employee e2 = new Employee("Hasan",1500,50,2012);
+        System.out.println(e1);
+        System.out.println("-------------------------------");
+        System.out.println(e2);
+    }
+
+}

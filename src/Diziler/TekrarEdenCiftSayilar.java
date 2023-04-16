37910fed4ef21259f5681760ca51d409b2b2b578
Onur Cura <onurcuraa@gmail.com>
2023-04-17 01:11:10 +0300

    Proje yapıldı.

--- src/Diziler/TekrarEdenCiftSayilar.java
+++ src/Diziler/TekrarEdenCiftSayilar.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 public class TekrarEdenCiftSayilar {
     public static void main(String[] args) {
-
         int[] list = {6, 0, 3, 2, 6, 5, 3, 5, 8, 14, 9, 8, 14, 8};
         Arrays.sort(list);
         int num = list[list.length - 1];
@@ -21,30 +20,3 @@ public class TekrarEdenCiftSayilar {
         }
     }
 }
-
-
-/*
-if (game[n + 1] == 0) {
-                n++;
-                if (n == game.length - 1) {
-                    break;
-                }
-            } else {
-                break;
-            }
- */
-
-/*for(int i = 0;i<game.length;){
-                 n = i;
-                 if (game[n+1]==0){
-                     i++;
-                 }else if(game[n+leap]==0){
-                     i +=leap;
-                 }else{
-                     break;
-                 }
-
-            }
-            break;
-        000110011011
- */
